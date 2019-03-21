ebc225c9285f8a3f24024301b1263603388a1ea9
piotrmilos <piotr.milos@codilime.com>
2019-03-21 11:26:24 +0100

    speedups hacks

--- tensor2tensor/rl/trainer_model_free.py
+++ tensor2tensor/rl/trainer_model_free.py
@@ -36,10 +36,210 @@ python -m tensor2tensor.rl.trainer_model_free \
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+import time
+_time = time.time()
+#
+# import os
+# import pprint
+#
+# import tensorflow as tf
+# import reprlib
+# import socket
+# import codecs
+# import xmlrpc
+# import os
+# import google_auth_httplib2
+# import pkgutil
+# import difflib
+# import stringprep
+# import struct
+# import random
+# import html
+# import signal
+# import importlib
+# import fractions
+# import gc
+# import gym
+# import ast
+# import keyword
+# import json
+# import time
+# import string
+# import cryptography
+# import calendar
+# import dis
+# import oauth2client
+# import inspect
+# import pwd
+# import threading
+# import heapq
+# import getpass
+# import posix
+# import uritemplate
+# import encodings
+# import numbers
+# import multiprocessing
+# import gettext
+# import array
+# import timeit
+# import pyasn1
+# import asn1crypto
+# import PIL
+# import ctypes
+# import subprocess
+# import sys
+# import fnmatch
+# import tempfile
+# import asyncio
+# import urllib
+# import tokenize
+# import queue
+# import httplib2
+# import pkg_resources
+# import operator
+# import http
+# import errno
+# import mesh_tensorflow
+# import getopt
+# import pydoc
+# import posixpath
+# import mmap
+# import genericpath
+# import decimal
+# import tensorflow_probability
+# import csv
+# import mimetypes
+# import tensorflow
+# import sre_parse
+# import sre_constants
+# import pyexpat
+# import uuid
+# import pprint
+# import abc
+# import zlib
+# import copyreg
+# import token
+# import traceback
+# import yaml
+# import fcntl
+# import bisect
+# import _decimal
+# import ssl
+# import logging
+# import pathlib
+# import cython_runtime
+# import copy
+# import zipimport
+# import astor
+# import lzma
+# import textwrap
+# import pickle
+# import idna
+# import quopri
+# import socketserver
+# import plistlib
+# import functools
+# import math
+# import zipfile
+# import cffi
+# import marshal
+# import hmac
+# import grp
+# import gast
+# import rsa
+# import codeop
+# import tensorboard
+# import googleapiclient
+# import xml
+# import cv2
+# import itertools
+# import stat
+# import glob
+# import code
+# import binascii
+# import unicodedata
+# import numpy
+# import email
+# import contextlib
+# import select
+# import selectors
+# import atexit
+# import re
+# import chardet
+# import tarfile
+# import certifi
+# import argparse
+# import concurrent
+# import six
+# import sysconfig
+# import pyasn1_modules
+# import bz2
+# import locale
+# import site
+# import shutil
+# import io
+# import termios
+# import h5py
+# import requests
+# import sre_compile
+# import enum
+# import scipy
+# import distutils
+# import ntpath
+# import urllib3
+# import hashlib
+# import opcode
+# import datetime
+# import warnings
+# import types
+# import uu
+# import collections
+# import platform
+# import linecache
+# import termcolor
+# import base64
+# import trace
+# import gzip
+# import ipaddress
+# import absl
+# import unittest
 import os
-import pprint
-
+# print("t0:{}".format(time.time() - _time))
+# from tensor2tensor.data_generators import all_problems
+# all_problems.ALL_MODULES = ['tensor2tensor.data_generators.gym_env']
+#
+# import sys
+# blacklist = []
+# blacklist.extend([
+#     'tensor2tensor.models.' + module
+#     for module in [
+#         'basic', 'bytenet', 'distillation', 'evolved_transformer', 'image_transformer', 'image_transformer_2d', 'lstm', 'mtf_image_transformer', 'mtf_resnet', 'mtf_transformer', 'mtf_transformer2', 'neural_gpu', 'resnet', 'revnet', 'shake_shake', 'slicenet', 'text_cnn', 'transformer', 'vanilla_gan', 'xception',
+#     ]
+# ])
+# blacklist.extend([
+#     'tensor2tensor.models.research.' + module
+#     for module in [
+#         'adafactor_experiments', 'aligned', 'attention_lm', 'attention_lm_moe', 'autoencoders', 'cycle_gan', 'gene_expression', 'glow', 'lm_experiments', 'moe_experiments', 'multiquery_paper', 'similarity_transformer', 'super_lm', 'transformer_moe', 'transformer_nat', 'transformer_parallel', 'transformer_revnet', 'transformer_sketch', 'transformer_symshard', 'transformer_vae', 'universal_transformer', 'vqa_attention', 'vqa_recurrent_self_attention', 'vqa_self_attention',
+#     ]
+# ])
+# blacklist.extend([
+#     'tensor2tensor.models.video.' + module
+#     for module in [
+#        'epva', 'next_frame_glow', 'savp',
+#     ]
+# ])
+# blacklist.append('tensor2tensor.layers.common_audio')
+# for module in blacklist:
+#   sys.modules[module] = object()
+#
+# class fake_text_encoder(object):
+#     def TextEncoder(*args, **kwargs):
+#         pass
+# sys.modules['tensor2tensor.data_generators.text_encoder'] = fake_text_encoder()
+#
+# class fake_bleu_hook(object):
+#     bleu_score = None
+# sys.modules['tensor2tensor.utils.bleu_hook'] = fake_bleu_hook()
 from tensor2tensor.models.research import rl
 from tensor2tensor.rl import rl_utils
 from tensor2tensor.utils import flags as t2t_flags  # pylint: disable=unused-import
